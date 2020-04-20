df521dfb11d4ac3dd0a3dd93797fe5e79edaeab9
Chris Hosler <cmstie@gmail.com>
2020-04-20 15:43:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,28 @@ def loop_message_five_times(message)
     count += 1
   end
 end
+
+def loop_message_n_times(message, integer)
+  count = 0
+  while count < integer do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    array[count].to_s.push(new_array)
+    count += 1 
+  end
+end
